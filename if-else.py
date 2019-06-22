f950552195d5832cd37529eaab6b77929a61314d
varunswing <varuns939@gmail.com>
2019-06-22 23:28:51 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,10 @@
+name = "Lucy"
+
+if name is "Varun":
+    print("What's up Varun")
+elif name is "Aman":
+    print("What's up Aman")
+elif name is "Rakesh":
+    print("What's up Rakesh")
+else :
+    print("Who are you?")
