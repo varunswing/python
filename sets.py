4a393e58a8fa5dbc3c3562628d0e17a07dff7614
varunswing <varuns939@gmail.com>
2019-06-24 00:01:49 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,7 @@
+groceries = {'milk', 'cereals', 'vegetables', 'fruits'}
+print(groceries)
+
+if 'milk' in groceries:
+    print("You have already buied it!")
+else :
+    print("Yes! you need that")
