97140fc0a29435825fa68b2ab32dc32a9b058bb5
varunswing <varuns939@gmail.com>
2019-06-24 00:01:21 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+groceries = {'Milk': ' is healthy', 'Cereals': ' have vitamins', 'Vegetables': ' have minerals', 'Fruits': ' have vitamins and minerals too'}
+
+print(groceries['Milk'])
+
+for k, v in groceries.items():
+    print(k + v)
