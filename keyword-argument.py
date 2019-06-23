8bebc323f087313b880cbf3360b3198a2c3cbdf8
varunswing <varuns939@gmail.com>
2019-06-24 00:01:34 +0530

    Create keyword-argument.py

---
+++ keyword-argument.py
@@ -0,0 +1,6 @@
+def dumb_sentance(name = "Varun", action = "ate", item = "apple"):
+    print(name, action, item)
+
+dumb_sentance()
+dumb_sentance("Vipul", "farts", "gently")
+dumb_sentance(item = "awesome", action = "is")
