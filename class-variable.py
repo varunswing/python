ad2a08faab74724a7ba53b0815621ffebb39d408
varunswing <varuns939@gmail.com>
2019-06-26 21:59:51 +0530

    Create class-variable.py

---
+++ class-variable.py
@@ -0,0 +1,14 @@
+class Girl:
+    gender = "Female"
+
+    def __init__(self, name):
+        self.name = name
+
+
+r = Girl("Kauski")
+e = Girl("Eshita")
+
+print(r.gender)
+print(r.name)
+print(e.gender)
+print(e.name)
